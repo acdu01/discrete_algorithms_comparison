f96c60cd1d2d23f95ddfeb980e6a35642a326260
etuthill <etuthillolin@gmail.com>
2025-10-23 11:19:43 -0400

    wrote friedman

---
+++ strategies/friedman.py
@@ -0,0 +1,11 @@
+from strategies.base_strategy import Strategy
+
+class DefectTitForTat(Strategy):
+    def move(self):
+        if not self.opponent_history:
+            return 'C'  # start with cooperation
+        if 'D' in self.opponent_history:
+            return 'D'  # if opponent has defected before, always defect
+        else:
+            return 'C'  # otherwise, cooperate
+    
