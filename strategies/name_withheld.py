8dce9ee228217fad85d187cdb117e9a446780b2a
etuthill <etuthillolin@gmail.com>
2025-10-27 10:57:09 -0400

    name withheld

---
+++ strategies/name_withheld.py
@@ -0,0 +1,28 @@
+from strategies.base_strategy import Strategy
+import random
+
+class NameWithheld(Strategy):
+    def __init__(self):
+        self.my_history = []
+        self.opponent_history = []   
+        self.probibility = 0.3 # start probility at 0.3
+        self.round = 0
+        self.random_detector = 0
+ 
+    def move(self): 
+        if self.random_detector == 1: # always defect if opponent is random
+            return 'D'
+        elif random.random() <= self.probibility: # cooperate based on probability
+            return 'C'
+       
+        fraction = self.opponent_history.count('D') / len(self.opponent_history) # fraction of opponent defections
+        
+        if self.round // 10 and self.opponent_history:   
+            if fraction >= 0.6 and self.probibility >= 0.1: 
+                self.probibility = self.probibility -0.1 # less cooperative if opponent defects a lot
+            elif fraction < 0.4 and self.probibility <= 0.9: # more cooperative if opponent cooperates a lot
+                self.probibility = self.probibility +0.1
+            else: 
+                self.random_detector = 1
+        
+        return 'D'
